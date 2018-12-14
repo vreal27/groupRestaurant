1759849ab22e337d5666510cd350878e6d2cdb75
Eddie Villareal <eddie.villareal27@gmail.com>
2018-12-14 14:39:21 -0800

    tab functionality working

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
+	$(".panel").hide();
+	$('.information').on('click','a', function(e){
+		e.preventDefault();
+		$('.information a').removeClass('active');
+		$(this).addClass('active');
+		$(this.hash).show().siblings().hide();
+	})
 
 })
